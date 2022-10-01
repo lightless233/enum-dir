b3577e08209b073450be9b3343adeaf2cc08c8d4
lightless <lightless@foxmail.com>
2022-10-01 15:59:44 +0800

    update github actions

---
+++ build/build.sh
@@ -0,0 +1,14 @@
+TARGET=$1
+echo "current target: $TARGET"
+PKG_NAME="${TARGET}.tar.gz"
+
+cross build --target "$TARGET" --release
+mkdir build_tmp
+
+if [[ "$TARGET" == *"-linux-"* ]]; then
+    tar czvf "build_tmp/$PKG_NAME" -C "./target/$TARGET/release/" enum-dir
+elif [[ "$TARGET" == *"-windows-"* ]]; then
+    tar czvf "build_tmp/$PKG_NAME" -C "./target/$TARGET/release/" enum-dir.exe
+fi
+
+shasum -a 256 "build_tmp/$PKG_NAME" > "build_tmp/$PKG_NAME.sha256"
